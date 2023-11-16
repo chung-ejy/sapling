fbef9fff6a798984e079a011f4e80500161ab6a9
chung.ejy <chung.ejy@gmail.com>
2023-11-16 18:52:57 -0500

    add initial methods

---
+++ extractor/alp_extractor.py
@@ -0,0 +1,23 @@
+import requests as r
+
+class ALPExtractor(object):
+
+    @classmethod
+    def prices(self,ticker,start,end):
+        return ""
+    
+    @classmethod
+    def account(self,ticker,start,end):
+        return ""
+    
+    @classmethod
+    def buy(self,ticker,qty):
+        return ""
+
+    @classmethod
+    def sell(self,ticker,qty):
+        return ""
+
+    @classmethod
+    def close(self):
+        return ""
