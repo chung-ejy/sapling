e176446ad549fe8f80e9a13d29c24df3fb941c5a
chung.ejy <chung.ejy@gmail.com>
2023-11-17 05:29:40 -0500

    complete

--- main_algo.py
+++ main_algo.py
@@ -8,31 +8,30 @@ import matplotlib.pyplot as plt
 from random import shuffle
 market = ADatabase("market")
 market.connect()
-sp500 = market.retrieve("sp100")
+sp500 = market.retrieve("sp500")
 market.disconnect()
 
 start = datetime.now() - timedelta(days=365) - timedelta(days=140)
 end = datetime.now() - timedelta(days=1)
 
-
+rolling_val = 50
 tickers = sp500["ticker"].values
 # tickers = pd.read_csv("tickers.csv")["ticker"]
 rr = 0.00
-std = 300
+std = 100
 sim = []
 shuffle(tickers)
-for ticker in tqdm(tickers[::10]):
+for ticker in tqdm(tickers[:10]):
     try:
         prices = p.column_date_processing(alp.prices(ticker,start,end))
-        prices["prediction"] = prices["adjclose"].rolling(100).mean()
-        prices["std"] = prices["adjclose"].rolling(50).std()
+        prices["prediction"] = prices["adjclose"].rolling(rolling_val).mean()
+        prices["std"] = prices["adjclose"].rolling(rolling_val).std()
         prices["signal"] = (prices["prediction"] - prices["adjclose"]) / prices["adjclose"]
         prices["abs"] = prices["signal"].abs()
         prices["direction"] = prices["signal"] / prices["abs"]
         prices["buy_price"] = prices["adjclose"].shift(-1)
         prices["buy_date"] = prices["date"].shift(-1)
-        # prices["sell_price"] = prices["adjclose"].shift(-5)
-        prices["sell_price"] = prices["adjclose"].shift(-1) * 0.95
+        prices["sell_price"] = prices["adjclose"].shift(-5)
         prices["sell_date"] = prices["date"].shift(-5)
         prices["return"] = (prices["sell_price"] - prices["buy_price"]) / prices["buy_price"] * prices["direction"] + 1
         sim.append(prices.iloc[100:].fillna(1))
@@ -41,10 +40,9 @@ for ticker in tqdm(tickers[::10]):
 
 simulation = pd.concat(sim)
 trades = simulation[simulation["weekday"]==4]
-trades = simulation[simulation["direction"]==1]
 trades = trades[trades["std"]<=std]
 trades = trades[trades["abs"]>=rr]
-trades = trades.sort_values("abs",ascending=True).groupby(["date"]).first().reset_index()
+trades = trades.sort_values("abs",ascending=False).groupby(["date"]).first().reset_index()
 trades.to_csv("trades.csv")
 trades["cr"] = trades["return"].cumprod() * 100
 trades.sort_values("date",inplace=True)
