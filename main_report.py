58c34cd120d4b0be26d413860875d0b793cbfd04
chung.ejy <chung.ejy@gmail.com>
2023-11-19 01:36:05 -0500

    randomized

--- main_report.py
+++ main_report.py
@@ -3,18 +3,22 @@ from database.adatabase import ADatabase
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+from random import shuffle
 db = ADatabase("algo")
 tickers = pd.read_csv("tickers.csv")["ticker"]
 
 skip = 1
 queries = []
-rr = 0.0
-risk = 0.10
-# rolling_val = 20
-# projection_week = 13
-rolling_val = 50
-projection_week = 1
+rrs = [0,0.1,0.2,0.3]
+rolling_vals = [10,20,50,100]
+projection_weeks = [1,2,4,13,26]
+shuffle(rrs)
+shuffle(rolling_vals)
+shuffle(projection_weeks)
+rr = rrs[0]
+risk = rrs[-1]
+rolling_val = rolling_vals[0]
+projection_week = projection_weeks[0]
 trades = Algo(rolling_val,projection_week,rr,risk,tickers,skip).algo()
 trades["avg_risk"] = trades["risk"].mean()
 plt.plot(trades["date"].values,trades["cr"].values)
