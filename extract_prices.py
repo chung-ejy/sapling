283be48650c5076ff023e806c92ed617091cfe51
chung.ejy <chung.ejy@gmail.com>
2023-12-01 02:49:55 -0500

    removed logs

--- extract_prices.py
+++ extract_prices.py
@@ -3,16 +3,16 @@ from extractor.tiingo_extractor import TiingoExtractor
 from datetime import datetime, timedelta
 from tqdm import tqdm
 market = ADatabase("market")
-start = datetime.now() - timedelta(days=365.25*20)
+start = datetime.now() - timedelta(days=365.25*14)
 end = datetime.now()
 
 market.connect()
-sp100 = market.retrieve("sp100")
+sp500 = market.retrieve("sp500")
 market.disconnect()
 
 market.connect()
 market.drop("prices")
-for ticker in tqdm(sp100["ticker"].values):
+for ticker in tqdm(sp500["ticker"].values):
     try:
         ticker_data = TiingoExtractor.prices(ticker,start,end)[["date","adjClose"]]
         ticker_data["ticker"] = ticker
