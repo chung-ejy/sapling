e7e06697b1c6adc0348c7511154ee7ab2e1ba990
chung.ejy <chung.ejy@gmail.com>
2023-12-05 20:50:09 -0500

    financial strategy

--- extract_filings.py
+++ extract_filings.py
@@ -1,19 +1,25 @@
-from database.adatabase import ADatabase
-from extractor.fmp_extractor import FMPExtractor
-from tqdm import tqdm
-market = ADatabase("market")
+import requests
+import zipfile
 
-market.connect()
-sp100 = market.retrieve("sp100")
-market.disconnect()
+for year in range(2009,2024):
+    for quarter in range(1,5):
+        print(year,quarter)
+        url = f"https://www.sec.gov/files/dera/data/financial-statement-data-sets/{year}q{quarter}.zip"
+        response = requests.get(url)
 
-market.connect()
-market.drop("balance_sheet")
-for ticker in tqdm(sp100["ticker"]):
-    try:
-        filings = FMPExtractor.balance_sheet(ticker)
-        market.store("balance_sheet",filings)
-    except Exception as e:
-        print(str(e))
-market.create_index("balance_sheet","ticker")
-market.disconnect()
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Open a file in write binary ('wb') mode
+            with open(f'./sec/{year}q{quarter}.zip', 'wb') as file:
+                # Write the file's content to the specified file
+                file.write(response.content)
+
+            # Open the downloaded zip file
+            with zipfile.ZipFile(f'./sec/{year}q{quarter}.zip', 'r') as zip_ref:
+                # Extract all the files in the zip file
+                zip_ref.extractall(f'./sec/{year}q{quarter}')
+
+            print("File downloaded and unzipped successfully.")
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
+            print(response.json())
