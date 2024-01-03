98d6fc07b5986d30b2cb470874a13deab1ac43c8
chung.ejy <chung.ejy@gmail.com>
2024-01-03 15:28:40 -0500

    debugging

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from backtest_functions.backtest_functions import BacktestFunctions as bf
 from strategy.strategy import Strategy
+import pandas as pd
+from tqdm import tqdm
 
-for name in Strategy._member_names_:
-    for i in range(60,65,5):
-        for position in range(20,25,5):
-            query = {
-                "strategy":name,
-                "holding_period":i,
-                "positions":position,
-                "stop_loss":0.03,
-                "ascending":False,
-            }
-            results = bf.backtest(query)
-            print(name,list(results.keys()))
+analysis = []
+for name in tqdm(Strategy._member_names_):
+    for ascending in [True,False]:
+        query = {
+            "strategy":name,
+            "holding_period":5,
+            "positions":10,
+            "stop_loss":0.03,
+            "ascending":ascending,
+        }
+        results = bf.backtest(query)
+        analysis.append({
+            "strat":name,
+            "cr":results["portfolio"][-1]["cumulative_return"]
+        })
+
+print(pd.DataFrame(analysis).sort_values("cr",ascending=False))
