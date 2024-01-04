fe73f57d7a16a8d512cf799695d0dc0bc77d5496
chung.ejy <chung.ejy@gmail.com>
2024-01-03 20:03:16 -0500

    added csrf exempt decorator

--- backtest/views.py
+++ backtest/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from rest_framework.decorators import api_view
 
+@csrf_exempt
 @api_view(['POST'])
 def backtestView(request):
     try:
