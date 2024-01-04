d90bb415f0f13f460c2f389f932184359af65376
chung.ejy <chung.ejy@gmail.com>
2024-01-04 18:25:04 -0500

    fixed return calculations

--- returns/returns.py
+++ returns/returns.py
@@ -8,7 +8,7 @@ class Returns(object):
         ticker_prices["sell_price"] = ticker_prices["adjclose"].shift(-5)
         ticker_prices["buy_date"] = [x + timedelta(days=1) if x.weekday() < 4 else x + timedelta(days=7-x.weekday()) for x in ticker_prices["date"]]
         ticker_prices["sell_date"] = ticker_prices["date"] + timedelta(days=sell_day_shift)
-        ticker_prices["return"] =  (ticker_prices["buy_price"] - ticker_prices["sell_price"]) / ticker_prices["sell_price"]
+        ticker_prices["return"] =  (ticker_prices["sell_price"] - ticker_prices["buy_price"]) / ticker_prices["buy_price"]
         ticker_prices["return"] = ticker_prices["return"] * (1/strategy.positions)
         ticker_prices["return"] = [max(float(-strategy.stop_loss/strategy.positions),x) for x in ticker_prices["return"]]
         return ticker_prices
