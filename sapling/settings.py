1cef562f47064b23bd71cfc3a4e56df8b09e13cd
chung.ejy <chung.ejy@gmail.com>
2024-01-04 18:52:36 -0500

    settings

--- sapling/settings.py
+++ sapling/settings.py
@@ -14,7 +14,10 @@ ALLOWED_HOSTS = [
                 'mistletoe.onrender.com'
 
                  ]
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    
 INSTALLED_APPS = [
     'backtest',
     'backtest_functions',
