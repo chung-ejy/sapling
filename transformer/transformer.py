4672a441e8c11a1f3d9ef747b22a7e80d7c13f5a
chung.ejy <chung.ejy@gmail.com>
2024-01-08 20:00:22 -0500

    transformer added

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -3,6 +3,7 @@ from database.adatabase import ADatabase
 from returns.returns import Returns
 import pandas as pd
 from tqdm import tqdm
+from extractor.alp_extractor import ALPExtractor
 
 class Transformer(object):
 
@@ -23,4 +24,21 @@ class Transformer(object):
                 print(str(e))
                 continue
         market.disconnect()
+        return pd.concat(prices)
+    
+    @classmethod
+    def cloud_transform(self,strategy,start,end):
+        prices = []
+        overhead = strategy.overhead()
+        for ticker in tqdm(strategy.tickers):
+            try:
+                ticker_prices = processor.column_date_processing(ALPExtractor.prices(ticker,start,end))
+                ticker_prices["ticker"] = ticker
+                ticker_prices.sort_values("date",inplace=True)
+                ticker_prices = strategy.signal(overhead,ticker_prices)
+                ticker_prices = Returns.returns(strategy,ticker_prices)
+                prices.append(ticker_prices)
+            except Exception as e:
+                print(str(e))
+                continue
         return pd.concat(prices)
