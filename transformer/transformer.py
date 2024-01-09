1cb5c686f9bae74799b329f9cf9dc52323e42511
chung.ejy <chung.ejy@gmail.com>
2024-01-08 19:59:20 -0500

    rebaseline

---
+++ transformer/transformer.py
@@ -0,0 +1,44 @@
+from processor.processor import Processor as processor
+from database.adatabase import ADatabase
+from returns.returns import Returns
+import pandas as pd
+from tqdm import tqdm
+from extractor.alp_extractor import ALPExtractor
+
+class Transformer(object):
+
+    @classmethod
+    def transform(self,strategy):
+        market = ADatabase("market")
+        market.cloud_connect()
+        prices = []
+        overhead = strategy.overhead()
+        for ticker in strategy.tickers:
+            try:
+                ticker_prices = processor.column_date_processing(market.query("prices",{"ticker":ticker}))
+                ticker_prices.sort_values("date",inplace=True)
+                ticker_prices = strategy.signal(overhead,ticker_prices)
+                ticker_prices = Returns.returns(strategy,ticker_prices)
+                prices.append(ticker_prices)
+            except Exception as e:
+                print(str(e))
+                continue
+        market.disconnect()
+        return pd.concat(prices)
+    
+    @classmethod
+    def cloud_transform(self,strategy,start,end):
+        prices = []
+        overhead = strategy.overhead()
+        for ticker in tqdm(strategy.tickers):
+            try:
+                ticker_prices = processor.column_date_processing(ALPExtractor.prices(ticker,start,end))
+                ticker_prices["ticker"] = ticker
+                ticker_prices.sort_values("date",inplace=True)
+                ticker_prices = strategy.signal(overhead,ticker_prices)
+                ticker_prices = Returns.returns(strategy,ticker_prices)
+                prices.append(ticker_prices)
+            except Exception as e:
+                print(str(e))
+                continue
+        return pd.concat(prices)
