36ae36ff161b4b9854c162e5cbb2f4efc2bc8488
chung.ejy <chung.ejy@gmail.com>
2024-01-14 20:55:22 -0500

    tweaks to btf

--- backtest_functions/backtest_functions.py
+++ backtest_functions/backtest_functions.py
@@ -3,15 +3,17 @@ from strategy.strategy_factory import StrategyFactory
 from transformer.transformer import Transformer
 from backtester.backtester import Backtester
 from processor.server_processor import ServerProcessor
-
+from datetime import datetime, timedelta
 class BacktestFunctions(object):
 
     @classmethod
     def backtest(self,query):
+        start = datetime.now() - timedelta(days=365.25*2)
+        end = datetime.now() - timedelta(hours=24)
         parameter = AParameter()
         parameter.build(query)
         strategy = StrategyFactory.build(parameter)
-        simulation = Transformer.cloud_transform(strategy)
+        simulation = Transformer.cloud_transform(strategy,start,end)
         trades = Backtester.backtest(strategy,simulation)
         portfolio = Backtester.portfolio(trades)
         recommendations = Backtester.recommendations(trades)
