6a7918834128746418196ebd75a681bf5a801d49
chung-ejy <chung.ejy@gmail.com>
2024-02-17 16:42:14 +0900

    double checking script

--- twine.py
+++ twine.py
@@ -20,6 +20,21 @@ keys = db.retrieve("secrets")
 parameter = db.retrieve("kpi").sort_values("return",ascending=False).iloc[0].to_dict()
 db.disconnect()
 
+for bot in bots.iterrows():
+    try:
+        user = bot[1]["username"]
+        live = bot[1]["live"]
+        user_keys = keys[keys["username"]==user].to_dict("records")[0]
+        secret = user_keys["secret"]
+        key = user_keys["key"]
+        alp_client = ALPClientExtractor(key,secret)
+        account = alp_client.account()
+        cash = float(account["cash"])
+        print(user,live,cash)
+    except Exception as e:
+        print(str(e))
+        continue
+
 if today.weekday() == 0:
     param = AParameter()
     param.build(parameter)
