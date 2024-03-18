d613d859192749c866f2b902e0bf353d2e6eebb1
chung-ejy <chung.ejy@gmail.com>
2024-03-19 07:08:27 +0900

    cb bot

--- coinbase_bot.py
+++ coinbase_bot.py
@@ -5,22 +5,38 @@ load_dotenv()
 from datetime import datetime, timedelta, timezone
 import pandas as pd
 import matplotlib.pyplot as plt
-client = CoinbaseClientExtractor(os.getenv("SEANCOINBASEKEY"),os.getenv("SEANCOINBASESECRET"))
+import warnings
+from time import sleep
+warnings.simplefilter(action="ignore")
+client = CoinbaseClientExtractor(os.getenv("ERICCOINBASEKEY"),os.getenv("ERICCOINBASESECRET"))
+tickers = ["BTC-USD","ETH-USD","SOL-USD","XRP-USD","AVAX-USD","ADA-USD"]
+cash = 100
+while True:
+    try:
+        start = datetime.now() - timedelta(hours=5)
+        end = datetime.now()
+        start_utc = start.astimezone(timezone.utc)
 
-start = datetime.now() - timedelta(hours=5)
-end = datetime.now()
-start_utc = start.astimezone(timezone.utc)
-start_unix_timestamp = (start_utc - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds()
-
-end_utc = end.astimezone(timezone.utc)
-end_unix_timestamp = (end_utc - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds()
-
-df = pd.DataFrame(client.client.get_candles("BTC-USD",int(start.timestamp()),int(end.timestamp()),"ONE_MINUTE")["candles"]).sort_values("start")
-df["close"] = [float(x) for x in df["close"]]
-df["bollinger"] = df["close"].rolling(15).mean() + df["close"].rolling(15).std()
-df["signal"] = df["bollinger"] > df["close"]
-df["signal"] = [1 if x == True else - 1 for x in df["signal"]]
-df["return"] = df["close"].pct_change(-5) * df["signal"] + 1
-trades = df.iloc[::5]
-trades["cr"] = trades["return"].cumprod()
-print(trades.tail())
+        end_utc = end.astimezone(timezone.utc)
+        prices = []
+        rolling_val = 60
+        holding_period = 5
+        for ticker in tickers:
+            try:
+                df = pd.DataFrame(client.client.get_candles(ticker,int(start_utc.timestamp()),int(end_utc.timestamp()),"ONE_MINUTE")["candles"]).sort_values("start")
+                df["close"] = [float(x) for x in df["close"]]
+                # df["signal"] = df["close"].rolling(rolling_val).std() / df["close"].rolling(rolling_val).mean()
+                df["signal"] = df["close"].pct_change(holding_period).rolling(rolling_val).mean()
+                df["return"] = df["close"].pct_change(-holding_period) + 1
+                df["ticker"] = ticker
+                prices.append(df)
+            except Exception as e:
+                print(ticker,str(e))
+        sim = pd.concat(prices)
+        trades = sim.iloc[::holding_period].sort_values("signal",ascending=False).groupby("start").first()
+        trades["cr"] = trades["return"].cumprod()
+        recommendation = trades.tail(1)
+        print("buy",recommendation["ticker"].item())
+    except Exception as e:
+        print(str(e))
+    sleep(60*5)
