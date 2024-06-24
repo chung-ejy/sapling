830d2ac8a9c587a37c7311cdf83bdf3edd0c8dca
chung-ejy <chung.ejy@gmail.com>
2024-06-24 21:30:27 +0900

    debugging

--- main.py
+++ main.py
@@ -234,8 +234,8 @@ for date in sim.sort_values("date")["date"].unique():
                             position["options"][j] = option
                             bond = Bond.buy(opportunity_row, bond, notional * 0.4)
                             position["bonds"][j] = bond
-                            positions[i] = position
-                portfolio["positions"] = positions
+                    positions[i] = position
+            portfolio["positions"] = positions
             if date == sim["date"].min():
                 for i in range(11):
                     notional = float(cash / 11 / number_of_stocks)
