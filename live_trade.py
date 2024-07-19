3e1a704662402653eaf3f58eafaf532ba6930ba7
chung-ejy <chung.ejy@gmail.com>
2024-07-19 10:02:24 -0400

    preventing day trading

--- live_trade.py
+++ live_trade.py
@@ -16,40 +16,38 @@ from tqdm import tqdm
 load_dotenv()
 db = ADatabase("sapling")
 
-while True:
-    try:
-        sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",attrs={"id":"constituents"})[0].rename(columns={"Symbol":"ticker"})
-        tickers = sp500["ticker"]
-        trading_client = AlpacaLiveClient()
-        strategy = AverageReturnStrategy(AParameter(1))
-        prices = processor.column_date_processing(trading_client.bar(tickers))
-        if prices.index.size > 0:
-            sim = []
-            chunks = [tickers[i:i + 25] for i in range(0, len(tickers), 25)]
-            for chunk in chunks:
-                try:
-                    ticker_data = ALPClientExtractor(key=os.getenv("APCAKEY"),secret=os.getenv("APCASECRET")).prices_bulk(",".join(chunk),datetime.now() - timedelta(days=150),datetime.now())
-                    sleep(1)
-                    for key in ticker_data["bars"].keys():
-                        price = pd.DataFrame(ticker_data["bars"][key]).rename(columns={"c":"adjclose","t":"date"})[["date","adjclose"]]
-                        price["ticker"] = key
-                        sim.append(price)
-                except Exception as e:
-                    print(str(e))
-            sim = pd.concat(sim)
-            datas = []
-            for ticker in tickers:
-                try:
-                    price = processor.column_date_processing(sim[sim["ticker"]==ticker]).sort_values("date")
-                    price["prev_return"] = price["adjclose"].pct_change(5)
-                    datas.append(price.iloc[-1].dropna())
-                except Exception as e:
-                    print(str(e))
-                    continue
-            stuff = pd.DataFrame(datas)
-            sim = strategy.preprocessing(stuff,prices)
-            trader = LiveTrader(trading_client=trading_client,strategy=strategy)
-            trader.trade(sim)
-    except Exception as e:
-        print(str(e))
-    sleep(60)
+try:
+    sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",attrs={"id":"constituents"})[0].rename(columns={"Symbol":"ticker"})
+    tickers = sp500["ticker"]
+    trading_client = AlpacaLiveClient()
+    strategy = AverageReturnStrategy(AParameter(1))
+    prices = processor.column_date_processing(trading_client.bar(tickers))
+    if prices.index.size > 0:
+        sim = []
+        chunks = [tickers[i:i + 25] for i in range(0, len(tickers), 25)]
+        for chunk in chunks:
+            try:
+                ticker_data = ALPClientExtractor(key=os.getenv("APCAKEY"),secret=os.getenv("APCASECRET")).prices_bulk(",".join(chunk),datetime.now() - timedelta(days=150),datetime.now())
+                sleep(1)
+                for key in ticker_data["bars"].keys():
+                    price = pd.DataFrame(ticker_data["bars"][key]).rename(columns={"c":"adjclose","t":"date"})[["date","adjclose"]]
+                    price["ticker"] = key
+                    sim.append(price)
+            except Exception as e:
+                print(str(e))
+        sim = pd.concat(sim)
+        datas = []
+        for ticker in tickers:
+            try:
+                price = processor.column_date_processing(sim[sim["ticker"]==ticker]).sort_values("date")
+                price["prev_return"] = price["adjclose"].pct_change(5)
+                datas.append(price.iloc[-1].dropna())
+            except Exception as e:
+                print(str(e))
+                continue
+        stuff = pd.DataFrame(datas)
+        sim = strategy.preprocessing(stuff,prices)
+        trader = LiveTrader(trading_client=trading_client,strategy=strategy)
+        trader.trade(sim)
+except Exception as e:
+    print(str(e))
