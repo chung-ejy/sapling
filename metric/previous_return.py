81f4fcf1c313a62b6854abd91ad39756ab141bf3
chung-ejy <chung.ejy@gmail.com>
2024-09-16 05:41:38 -0400

    updated metric

--- metric/previous_return.py
+++ metric/previous_return.py
@@ -6,7 +6,7 @@ class PreviousReturn(AMetric):
     def __init__(self):
         super().__init__("previous_return")
         self.ascending =  True
-        self.positions = 10
+        self.positions = 1
         self.holding_period = 6
     
     def create_metric(self,price:pd.DataFrame):
