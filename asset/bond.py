f3122ef0f2f1bcc57c26656487117a6d3ebea6de
chung-ejy <chung.ejy@gmail.com>
2024-09-21 11:24:49 -0400

    multi asset simulation

--- asset/bond.py
+++ asset/bond.py
@@ -9,11 +9,14 @@ class Bond(object):
     @classmethod
     def sell(self,row,asset):
         updated = asset.copy()
+        updated["sell_date"] = row["date"]
         return updated
     
     @classmethod
     def buy(self,row,asset,notional):
         updated = asset.copy()
+        updated["buy_date"] = row["date"]
+        updated["sell_date"] = row["date"]
         updated["adjclose"] = 1000 / (1 + row["rf"])**10
         updated["quantity"] = notional / updated["adjclose"]
         updated["rate"] = row["rf"]
