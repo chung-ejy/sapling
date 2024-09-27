3e77b5278a04adad23192b67d1eeff5f7e4657ad
chung-ejy <chung.ejy@gmail.com>
2024-09-27 05:53:58 -0400

    what

--- metric/coefficient_of_variance.py
+++ metric/coefficient_of_variance.py
@@ -5,7 +5,7 @@ class CoefficientOfVariance(AMetric):
 
     def __init__(self):
         super().__init__("coefficient_of_variance")
-        self.ascending =  True
+        self.ascending =  False
         self.positions = 10
         self.holding_period = 4
     
