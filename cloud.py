c87c67bfac7bfa2970da3236441e12c36f43188d
chung.ejy <chung.ejy@gmail.com>
2024-10-09 13:19:23 -0400

    stuff

---
+++ cloud.py
@@ -0,0 +1,228 @@
+from database.adatabase import ADatabase
+from extractor.alp_client_extractor import ALPClientExtractor
+from extractor.fred_extractor import FREDExtractor
+from asset.stock import Stock
+import copy
+from processor.processor import Processor as processor
+from datetime import datetime, timedelta
+from tqdm import tqdm 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import pandas as pd
+from time import sleep
+import numpy as np
+
+end = datetime.now()
+start = datetime.now() - timedelta(days=365.25*2)
+market = ADatabase("market")
+
+
+russell1000 = pd.read_html("https://en.wikipedia.org/wiki/Russell_1000_Index")[2].rename(columns={"Symbol":"ticker"})
+tickers = []
+tickers.extend(russell1000["ticker"].values)
+market.cloud_connect()
+market.drop("prices")
+for ticker in tqdm(tickers):
+    try:
+        ticker_data = ALPClientExtractor(os.getenv("APCAKEY"),os.getenv("APCASECRET")).prices(ticker,start,end)
+        sleep(1)
+        ticker_data["ticker"] = ticker
+        market.store("prices",ticker_data)
+    except Exception as e:
+        print(str(e))
+market.create_index("prices","ticker")
+market.disconnect()
+
+fred = ADatabase("fred")
+sp500 = FREDExtractor.sp500(start,end)
+market_yield = FREDExtractor.market_yield(start,end)
+fred.cloud_connect()
+fred.drop("sp500")
+fred.drop("market_yield")
+fred.store("sp500",sp500)
+fred.store("market_yield",market_yield)
+fred.disconnect()
+
+sapling = ADatabase("sapling")
+
+sp500 = pd.read_html("https://en.wikipedia.org/wiki/Russell_1000_Index")[2].rename(columns={"Symbol":"ticker"})
+
+
+fred.cloud_connect()
+market_yield = fred.retrieve("market_yield")
+market_yield = market_yield.rename(columns={"value":"rf"})
+market_yield["rf"] = market_yield["rf"].replace(".",np.nan)
+market_yield.dropna(inplace=True)
+market_yield["rf"] = [float(x)/100 for x in market_yield["rf"]]
+market_yield["date"] = market_yield["date"].shift(-5)
+market_yield = processor.column_date_processing(market_yield)
+spy = fred.retrieve("sp500")
+spy = spy.rename(columns={"value":"spy"})
+spy["spy"] = spy["spy"].replace(".",np.nan)
+spy.dropna(inplace=True)
+spy["spy"] = [float(x) for x in spy["spy"]]
+spy["date"] = spy["date"].shift(-5)
+spy = processor.column_date_processing(spy)
+fred.disconnect()
+
+prices = []
+market.cloud_connect()
+for ticker in tqdm(sp500["ticker"].unique()):
+    try:
+        price = processor.column_date_processing(market.query("prices",{"ticker":ticker}))
+        price.sort_values("date",inplace=True)
+        price = price.merge(spy[["date","spy"]],on="date",how="left")
+        price = price.merge(market_yield[["date","rf"]],on="date",how="left")
+        price = price.merge(sp500[["ticker","GICS Sector"]],on="ticker",how="left")
+        price["prev_return"] = price["adjclose"].pct_change(5)
+        price["historical_return"] = price["adjclose"].pct_change(5)
+        price["factor_return"] = price["spy"].pct_change(5)
+        price["cov"] = price["factor_return"].rolling(100).cov(price["historical_return"])
+        price["var"] = price["factor_return"].rolling(100).var()
+        price["beta"] = price["cov"] / price["var"]
+        price["excess_return"] = price["rf"] + price["beta"] * (price["factor_return"] - price["rf"])
+        prices.append(price)
+    except Exception as e:
+        print(ticker,str(e))
+        continue
+market.disconnect()
+
+
+sim = pd.concat(prices)
+sim = sim[sim["date"]>datetime(datetime.now().year,1,1)]
+sim = sim[["date","ticker","adjclose","GICS Sector","prev_return","excess_return","rf"]].dropna()
+sim["quarter"] = [x.quarter for x in sim["date"]]
+
+# Initialize variables
+start = sim["date"].min()
+end = sim["date"].max()
+date = start
+number_of_stocks = 1
+portfolio = {
+    "date": date,
+    "tax": 0,
+    "fees": 0,
+    "cash": 100000,
+    "positions": [
+        {
+            "stocks": [{"ticker": "", "adjclose": 0, "quantity": 0} for _ in range(number_of_stocks)],
+        }
+        for _ in sp500["GICS Sector"].unique()
+    ]
+}
+
+states = []
+trades = []
+annual_balance = 100000
+prev_quarter = 0
+quarter = 1
+for date in tqdm(sim.sort_values("date")["date"].unique()):
+    today = sim[sim["date"] == date].copy()
+    prev_quarter = quarter
+    quarter = date.quarter
+    if today.index.size > 0:
+        try:
+            portfolio = copy.deepcopy(portfolio)
+            cash = portfolio["cash"]
+            portfolio["date"] = date
+            positions = copy.deepcopy(portfolio["positions"])
+            for i in range(11):
+                position = positions[i]
+                sector = list(sim["GICS Sector"].unique())[i]
+                stocks = copy.deepcopy(position["stocks"])
+                for j in range(number_of_stocks):
+                    stock = stocks[j]
+                    ticker = stock["ticker"]
+                    if ticker != "":
+                        row = today[today["ticker"] == ticker].iloc[0]
+                        stock = Stock.update(row, stock)
+                        position["stocks"][j] = stock
+                positions[i] = position
+            portfolio["positions"] = positions
+            
+            cash = portfolio["cash"]
+            positions = copy.deepcopy(portfolio["positions"])
+            
+            for i in range(11):
+                position = positions[i]
+                sector = list(sim["GICS Sector"].unique())[i]
+                stocks = copy.deepcopy(position["stocks"])
+                
+                for j in range(number_of_stocks):
+                    stock = stocks[j]
+                    ticker = stock["ticker"]
+                    if ticker != "" and quarter != prev_quarter:
+                        notional = stock["pv"]
+                        opportunity_row = today[today["GICS Sector"] == sector].sort_values("excess_return", ascending=True).iloc[j]
+                        opportunity_ticker = opportunity_row["ticker"]
+                        if (opportunity_ticker != ticker):
+                            row = today[today["ticker"] == ticker].iloc[0]
+                            stock = Stock.sell(row, stock)
+                            position["stocks"][j] = stock
+                            trades.append(stock)
+                            stock = Stock.buy(opportunity_row, stock, notional)
+                            position["stocks"][j] = stock
+                            positions[i] = position
+                portfolio["positions"] = positions
+            if date == sim["date"].min():
+                for i in range(11):
+                    notional = float(cash / 11 / number_of_stocks)
+                    position = positions[i]
+                    sector = list(sim["GICS Sector"].unique())[i]
+                    stocks = copy.deepcopy(position["stocks"])
+                    for j in range(number_of_stocks):
+                        stock = stocks[j]
+                        row = today[today["GICS Sector"] == sector].sort_values("excess_return", ascending=True).iloc[j]
+                        stock = Stock.buy(row, stock, notional)
+                        position["stocks"][j] = stock
+                    positions[i] = position
+                portfolio["positions"] = positions
+                portfolio["cash"] = 0
+            states.append(copy.deepcopy(portfolio))
+        except Exception as e:
+            print(f"Error on date {date}: {str(e)}")
+            continue
+
+portfolios = []
+for state in states:
+    for i in range(11):
+        for j in range(number_of_stocks):
+            view = state["positions"][i]["stocks"][j]
+            view["date"] = state["date"]
+            portfolios.append(state["positions"][i]["stocks"][j])
+
+for state in states:
+    try:
+        state["stock_pv"] = sum([sum([state["positions"][i]["stocks"][j]["pv"] for j in range(number_of_stocks)]) for i in range(11)])
+        state["pv"] = state["cash"] + state["stock_pv"]
+    except Exception as e:
+        print(str(e))
+        continue
+
+performance = pd.DataFrame(states).dropna()
+performance["stock_return"] = (performance["stock_pv"] - performance["stock_pv"].iloc[0]) / performance["stock_pv"].iloc[0]
+performance["return"] = (performance["pv"] - performance["pv"].iloc[0]) / performance["pv"].iloc[0]
+
+visualization = performance.merge(spy,on="date",how="left")
+visualization = visualization.merge(market_yield[["date","rf"]],on="date",how="left")
+
+visualization["return"] = (visualization["pv"] - visualization["pv"].iloc[0]) / visualization["pv"].iloc[0]
+visualization["benchmark_return"] = (visualization["spy"] - visualization["spy"].iloc[0]) / visualization["spy"].iloc[0]
+visualization["ir_return"] = (visualization["rf"] - visualization["rf"].iloc[0]) / visualization["rf"].iloc[0]
+
+
+
+(visualization["return"].iloc[-1] - visualization["benchmark_return"].iloc[-1]) / visualization["return"].std()
+
+t = pd.DataFrame(trades).merge(sp500[["ticker","GICS Sector"]],on="ticker",how="left")
+t["return"] = (t["adjclose"] - t["buy_price"]) / t["buy_price"]
+t["hpr"] = t["sell_date"] - t["buy_date"]
+t["hpr"] = [int(x.days) for x in t["hpr"]]
+
+sapling.cloud_connect()
+sapling.drop("trades")
+sapling.store("trades",t)
+sapling.drop("visualization")
+sapling.store("visualization",visualization)
+sapling.disconnect()
