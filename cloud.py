287a2b34ca416652a390c58490b8feb09010484f
chung.ejy <chung.ejy@gmail.com>
2024-10-10 07:46:12 -0400

    debugging

--- cloud.py
+++ cloud.py
@@ -153,15 +153,16 @@ for date in tqdm(sim.sort_values("date")["date"].unique()):
                 for j in range(number_of_stocks):
                     stock = stocks[j]
                     ticker = stock["ticker"]
-                    if ticker != "" and opportunity_ticker != ticker:
+                    if ticker != "":
                         notional = stock["pv"]
                         opportunity_row = today[today["GICS Sector"] == sector].sort_values("excess_return", ascending=True).iloc[j]
                         opportunity_ticker = opportunity_row["ticker"]
-                        row = today[today["ticker"] == ticker].iloc[0]
-                        stock = Stock.sell(row, stock)
-                        position["stocks"][j] = stock
-                        trades.append(stock)
-                        stock = Stock.buy(opportunity_row, stock, notional)
+                        if opportunity_ticker != ticker:
+                            row = today[today["ticker"] == ticker].iloc[0]
+                            stock = Stock.sell(row, stock)
+                            position["stocks"][j] = stock
+                            trades.append(stock)
+                            stock = Stock.buy(opportunity_row, stock, notional)
                         position["stocks"][j] = stock
                         positions[i] = position
                 portfolio["positions"] = positions
@@ -224,5 +225,5 @@ sapling.cloud_connect()
 sapling.drop("trades")
 sapling.store("trades",t)
 sapling.drop("visualization")
-sapling.store("visualization",visualization)
+sapling.store("visualization",visualization[["date","return","benchmark_return","ir_return"]])
 sapling.disconnect()
